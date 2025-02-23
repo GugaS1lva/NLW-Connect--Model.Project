7e0ba00edfe7eb477b11dec71c618d0cabdfd8bc
Gustavo Silva <contatogustavosilva017@outlook.com>
2025-02-23 16:43:01 -0300

    No Composition Pattern.
    O componente de Input possui 3 grandes elementos, cada um com sua própria funcionalidade interna. É um exemplo perfeito para utilizarmos o Composition Pattern.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { ArrowRight, Copy } from 'lucide-react'
 import Button from "./components/button";
 import IconButton from './components/icon-button';
+import Input from './components/input';
 
 export default function Home() {
   return (
@@ -13,6 +14,9 @@ export default function Home() {
       <IconButton>
         <Copy />
       </IconButton>
+
+      <Input type='email' placeholder='Digite seu email' />
+      <Input type='email' placeholder='Digite seu email' error />
     </main>
   );
 }
